7659915f18e4262f525932e736c496111cdc104d
ACEGAMER9 <88842865+ACEGAMER9@users.noreply.github.com>
2022-05-08 10:34:17 +0700

    delet and update
    delete
    - static.css file
    - base.html file
    update
    -login to show users login

--- flask-tutorial/flaskr/auth.py
+++ flask-tutorial/flaskr/auth.py
@@ -59,13 +59,14 @@ def login():
 
         if user is None:
             error = 'Incorrect username.'
+            
         elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
 
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect(url_for('index'))
+            return redirect(url_for('auth.Home'))
 
         flash(error)
 
@@ -89,7 +90,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for('auth.Home'))
 
 ########## REQUEST LOGIN ##########
 
