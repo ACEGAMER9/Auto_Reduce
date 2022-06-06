4edc8b8c19a41a2bbc0a141b9f57133b7b67383c
Acegame <88842865+ACEGAMER9@users.noreply.github.com>
2022-06-06 21:54:56 +0700

    Update
    - install.ps1
    - home.html

--- flask-tutorial/install.ps1
+++ flask-tutorial/install.ps1
@@ -1,8 +1,9 @@
 cd flask-tutorial
+python -m flask init-db
 pip install -r requirements.txt
 python3 -m venv venv
-.\venv\Scripts\activate
 $env:FLASK_APP = "flaskr"
 $env:FLASK_ENV = "development"
+python -m flask init-db
 [system.Diagnostics.Process]::Start("chrome","http://127.0.0.1:5000")
-flask run
+python -m flask run
